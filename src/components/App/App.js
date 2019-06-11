e15bc8442491ed618a939de8ce6d5457476d237f
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-06-11 17:13:01 -0400

    Start customizing boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React   from 'react';
+import Counter from '../Counter/Counter';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
